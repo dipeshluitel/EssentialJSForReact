24a149864c3d5a6ff76988ade86b4fdf223ef436
Dipesh <dipeshluitel40@gmail.com>
2026-03-04 22:25:15 +0545

    nullish operator implemented

---
+++ nullish.js
@@ -0,0 +1,15 @@
+// nullish coalescing Operator
+
+const user_age = null
+
+let result = user_age ?? "Age not provided";
+
+console.log(result)
+
+
+
+const user_age2 = 22
+
+let result2 = user_age2 ?? "Age not provided";
+
+console.log(result2)
