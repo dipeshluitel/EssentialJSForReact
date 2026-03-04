6cd81fc1dc682f6a13cd9770334415556fd1fc1b
Dipesh <dipeshluitel40@gmail.com>
2026-03-05 05:31:43 +0545

    falsy operator revised

--- nullish.js
+++ nullish.js
@@ -8,8 +8,17 @@ console.log(result)
 
 
 
-const user_age2 = 22
+const user_age2 = 0
 
 let result2 = user_age2 ?? "Age not provided";
 
 console.log(result2)
+
+
+// falsy Operator
+
+const user_age3 = 0
+
+let result3 = user_age3 || "Age not provided";
+
+console.log(result3)
